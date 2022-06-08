bd1ef41cf3f8c95deee789b7374cf983df7580b0
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-06-08 17:53:21 -0600

    Initial commit

---
+++ codeChallenge.js
@@ -0,0 +1,9 @@
+var goodDrivingRecord = true
+var isOver25 = true
+
+if(goodDrivingRecord = true && isOver25 = true){
+  console.log("Discounted car rental");
+}else if(goodDrivingRecord = true || isOver25 = true){
+  console.log("Pay full Price");
+}else{
+  console.log("Need a co-signer");
